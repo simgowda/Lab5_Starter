43d93098895d050cb81d8bbd3f30f0bbe13cf0ed
simgowda <simargowda@gmail.com>
2026-05-05 18:44:43 -0700

    fixed error

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,11 +1,11 @@
 // sum.test.js
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 with sum() to equal 3', () => {
-  expect(sum(1, 2)).toBe(4);
+  expect(sum(1, 2)).toBe(3);
 });
